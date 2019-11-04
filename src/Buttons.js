a382734b5f1430ef11cd83e94216b938cb44acf4
rodrigograca31 <rodrigograca1993@gmail.com>
2019-11-04 14:56:24 +0000

    Stretch: Handler Function :heavy_check_mark:

--- src/Buttons.js
+++ src/Buttons.js
@@ -2,17 +2,27 @@ import React from 'react';
 
 
 const Buttons = (props) => {
+
+	const incrementTeam = (team, value = 0) => {
+		console.log("incrementing");
+
+		if(team === "lions") {
+			props.setLionsScore({ home: props.lionsScore.home + value })
+		} else {
+			props.setTigersScore({ away: props.tigersScore.away + value })
+		}
+	}
+
 	// number = 4;
 	return (
 		<section className="buttons">
 			<div className="homeButtons">
-				{/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-				<button className="homeButtons__touchdown" onClick={() => props.setLionsScore({ home: props.lionsScore.home + 7 })}>Home Touchdown</button>
-				<button className="homeButtons__fieldGoal" onClick={() => props.setLionsScore({ home: props.lionsScore.home + 3 })}>Home Field Goal</button>
+				<button className="homeButtons__touchdown" onClick={() => incrementTeam("lions", 7)}>Home Touchdown</button>
+				<button className="homeButtons__fieldGoal" onClick={() => incrementTeam("lions", 3)}>Home Field Goal</button>
 			</div>
 			<div className="awayButtons">
-				<button className="awayButtons__touchdown" onClick={() => props.setTigersScore({ away: props.tigersScore.away + 7 })}>Away Touchdown</button>
-				<button className="awayButtons__fieldGoal" onClick={() => props.setTigersScore({ away: props.tigersScore.away + 3 })}>Away Field Goal</button>
+				<button className="awayButtons__touchdown" onClick={() => incrementTeam("tigers", 7)}>Away Touchdown</button>
+				<button className="awayButtons__fieldGoal" onClick={() => incrementTeam("tigers", 3)}>Away Field Goal</button>
 			</div>
 		</section>
 	);
